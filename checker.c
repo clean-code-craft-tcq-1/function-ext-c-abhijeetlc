aea90ce9163d734101d95926c48d5f8ea0a06a2b
Abhijeet Chitagubbi <77390129+abhijeetlc@users.noreply.github.com>
2021-03-11 19:02:55 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -103,7 +103,7 @@ int checkBattery(float TempIn, float SOCIn, float chargeRate) {
 int main() {
 	
   assert(checkBattery(25, 70, 0.6));
-  assert(!checkBattery(50, 85, 0));
+  assert(checkBattery(50, 85, 0));
   assert(!checkBattery(0, 0, 0));
   assert(!checkBattery(100, 100, 100));
   assert(checkBattery(30, 40, 0.5));
